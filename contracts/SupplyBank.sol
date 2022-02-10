98bf9d170c16ec73948b91d3ed721b84ee2ba10d
kingridda <abdelghaniridda01@gmail.com>
2022-02-10 03:55:38 +0100

    updating bank contract

--- contracts/SupplyBank.sol
+++ contracts/SupplyBank.sol
@@ -11,7 +11,6 @@ interface cETH {
     //@dev functions from Compound that are going to be used
     function mint() external payable; // to deposit to compound
     function redeem(uint redeemTokens) external returns (uint); // to withdraw from compound
-    function redeemUnderlying(uint redeemAmount) external returns (uint); // Redeem specified Amount
     //following 2 functions to determine how much you'll be able to withdraw
     function exchangeRateStored() external view returns (uint); 
     function balanceOf(address owner) external view returns (uint256 balance);
@@ -43,17 +42,19 @@ contract SupplyBank {
     
     // CETH contract at Ropsten ethereum testnet
     address COMPOUND_CETH_ADDRESS_ROPSTEN = 0x859e9d8a4edadfEDb5A2fF311243af80F85A91b8;
-    address SUPPLY_TOKEN_ADDRESS_ROPSTEN = 0xd1dC374975EabD303256B081E00b39CAec1897d0;
+    address SUPPLY_COIN_ADDRESS_ROPSTEN = 0x61EAc9bF571782c62262a730F07A107f594F1c21;
     cETH ceth = cETH(COMPOUND_CETH_ADDRESS_ROPSTEN);
-    IERC20 splt = IERC20(SUPPLY_TOKEN_ADDRESS_ROPSTEN);
 
-    function getContractBalance() public view returns(uint){
+    IERC20 splCoin = IERC20(SUPPLY_COIN_ADDRESS_ROPSTEN);
+
+    function getContractCEthBalance() public view returns(uint){
         return totalContractBalance;
     }
+    function getContractEthBalance() public view returns(uint){
+        return totalContractBalance*ceth.exchangeRateStored()/1e18;
+    }
     
-    mapping(address => uint) balances;
-    mapping(address => uint) ethBalances; // Eth Balance
-    mapping(address => uint) erc20Balances; // ERC20 Balance
+
     receive() external payable{}
     
     /**
@@ -63,27 +64,6 @@ contract SupplyBank {
         _mint(msg.value);
     }
 
-    /**
-    * @dev get the amount of stored Ether
-    */
-    function getBalance(address userAddress) public view returns(uint256) {
-        // get cETH of user then multiply by exchange rate to get Eth balance
-        return balances[userAddress]*ceth.exchangeRateStored()/1e18;
-    }
-
-    /**
-    * @dev get cETH Balance 
-    */
-    function getCethBalance(address userAddress) public view returns(uint256) {
-        return balances[userAddress];
-    }
-
-    /**
-    * @dev get Deposited/Withdrawn ETH amount
-    */
-    function getEthBalance(address userAddress) public view returns(uint256) {
-        return ethBalances[userAddress];
-    }
 
     /**
     * @dev Mints an specific amount of cEth
@@ -99,57 +79,41 @@ contract SupplyBank {
         
         uint cEthUser = cEthAfterMint - cEthBeforeMint;
         totalContractBalance +=cEthUser;
-        splt.mineForCEthHolder(msg.sender, cEthUser);
+        splCoin.mineForCEthHolder(msg.sender, cEthUser);
     }
 
-    function mineForCLT()external{
-        splt.mineForCEthHolder(msg.sender, 10*1e8);
-    }
-
-
-    /**
-    * @dev Withdraws all the Ether
-    */
-    function withdrawMax() public payable {
-        address payable transferTo = payable(msg.sender); // get payable to transfer towards
-        ceth.redeem(balances[msg.sender]); // Redeem that cETH
-        uint256 amountToWithdraw = getBalance(msg.sender); // Avalaible amount of $ that can be Withdrawn
-        totalContractBalance -= balances[msg.sender];
-        balances[msg.sender] = 0;
-        transferTo.transfer(amountToWithdraw);
-    }
     /**
     * @dev Withdraw a specific amount of Ether
     */
-    function withdrawAmount(uint amountRequested) public payable {
-        require(amountRequested <= getBalance(msg.sender), "Your balance is smaller than the requested amount");
-        address payable transferTo = payable(msg.sender); // get payable to transfer to sender's address
-        
-        uint256 cEthWithdrawn = _withdrawCEther(amountRequested);
+    function withdrawAmount(uint tokenAmount) public payable {
+        splCoin.transferFrom(msg.sender, address(this), tokenAmount);//needs approval
+        uint256 EthWithdrawn = _withdrawCEther(tokenAmount);//cETH = TOKEN AMOUNT
+        totalContractBalance = ceth.balanceOf(address(this));
 
-        totalContractBalance -= cEthWithdrawn;
-        balances[msg.sender] -= cEthWithdrawn;
-        transferTo.transfer(amountRequested);
+        address payable transferTo = payable(msg.sender); // get payable to transfer to sender's address
+        transferTo.transfer(EthWithdrawn);
         
     }
-
-    
     /**
     * @dev Redeems cETH for withdraw
     * @return Withdrawn cETH
     */
-    function _withdrawCEther(uint256 _amountOfEth) internal returns (uint256) {
-        uint256 cEthContractBefore = ceth.balanceOf(address(this));
-        ceth.redeemUnderlying(_amountOfEth);
-        uint256 cEthContractAfter = ceth.balanceOf(address(this));
+    function _withdrawCEther(uint256 _amountOfCEth) internal returns (uint256) {
+     //   uint256 cEthContractBefore = ceth.balanceOf(address(this));
+        uint256 EthContractBefore = address(this).balance;
 
-        uint256 cEthWithdrawn = cEthContractBefore - cEthContractAfter;
+        ceth.redeem(_amountOfCEth);
 
-        return cEthWithdrawn;
+     //   uint256 cEthContractAfter = ceth.balanceOf(address(this));
+        uint256 EthContractAfter = address(this).balance;
+
+      //  uint256 cEthWithdrawn = cEthContractBefore - cEthContractAfter;
+        uint256 EthWithdrawn = EthContractAfter - EthContractBefore;
+        return EthWithdrawn;
     }
 
 
-    function paySupplyWithSPLT(address to, uint amount) external{
-        splt.transferFrom(msg.sender, to, amount);
+    function paySupplierWithSPL(address to, uint amount) external{
+        splCoin.transferFrom(msg.sender, to, amount);
     }
 }
