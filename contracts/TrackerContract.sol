a0ebd42176c08f0d12ddf7f496f5950de42f8a88
kingridda <abdelghaniridda01@gmail.com>
2022-02-10 03:54:24 +0100

    updating tracker contract

--- contracts/TrackerContract.sol
+++ contracts/TrackerContract.sol
@@ -21,8 +21,8 @@ contract TrackerContract {
         uint256 createdAt;
     }
     struct Transitioner{
-        string name; // FDA
-        string description;   // fda dyal dwa
+        string name;
+        string description;   
         uint256 createdAt;
     }
     struct Transition{
@@ -30,7 +30,6 @@ contract TrackerContract {
         bool decision;
         uint256 createdAt;
     }
-    // dwa, dwa description, [{}, {addressFDA -> Trqnsitioner, true}]
     struct Item{
         string name;
         string description;
@@ -46,10 +45,10 @@ contract TrackerContract {
         manufacturers[msg.sender] = Manufacturer("Mname", "Mdesc", block.timestamp);
         Item storage it = items["product00"];
         it.name = "product name";
-        it.description = "product description";
+        it.description = "product long description";
         it.createdAt = block.timestamp;
         it.transitions.push(Transition(msg.sender, true, block.timestamp));
-
+        it.transitions.push(Transition(msg.sender, true, block.timestamp));
     }
     //calldata is constant
 
@@ -62,18 +61,25 @@ contract TrackerContract {
     function getItem(string memory id) external view returns(Item memory){
         return items[id];
     }
-    function addManufacturer(address id, Manufacturer calldata manufacturer)external onlyAdmin{
-        manufacturers[id] = manufacturer;
+    function addManufacturer(address id, string memory name, string memory description)external onlyAdmin{
+        Manufacturer storage man = manufacturers[id];
+        man.name = name;
+        man.description = description;
+        man.createdAt = block.timestamp;
     }
-    function addTransitioner(address id, Transitioner calldata transitioner)external onlyAdmin{
-        transitioners[id] = transitioner;
+    function addTransitioner(address id, string memory name, string memory description)external onlyAdmin{
+        Transitioner storage tr = transitioners[id];
+        tr.name = name;
+        tr.description = description;
+        tr.createdAt = block.timestamp;
     }
-    function addItem(string memory id, string memory name,string memory description)external {
+    function addItem(string memory id, string memory name, string memory description)external {
         Item storage it = items[id];
         it.name = name;
         it.description = description;
         it.createdAt = block.timestamp;
         it.transitions.push(Transition(msg.sender, true, block.timestamp));
+    
     }
     function addTransition(string memory id, bool decision)external{
         items[id].transitions.push(Transition(msg.sender, decision, block.timestamp));
